712f3da301ad0b6adc4daa274383bab9c0180ca6
Ítalo Holanda <italoholanda@gmail.com.br>
2022-09-19 17:52:42 -0300

    feat: specification route

---
+++ src/modules/cars/repositories/specification.repository.ts
@@ -0,0 +1,36 @@
+import Specification from "../model/Specification";
+import ISpecificationRepository, {
+  ISpecificationDTO,
+} from "./ISpecificationRepository";
+
+class SpecificationRepository implements ISpecificationRepository {
+  private categories: Specification[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  findByName(name: string) {
+    return this.categories.find(
+      (category) => category.name.toLowerCase() === name.toLowerCase()
+    );
+  }
+
+  create({ name, description }: ISpecificationDTO) {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(specification);
+  }
+
+  list() {
+    return this.categories;
+  }
+}
+
+export default SpecificationRepository;
---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from "express";
+
+import SpecificationRepository from "../modules/cars/repositories/specification.repository";
+import CreateSpecificationService from "../modules/cars/services/CreateSpecificationService";
+
+const specificationRoutes = Router();
+
+const specificationRepository = new SpecificationRepository();
+
+specificationRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  try {
+    const service = new CreateSpecificationService(specificationRepository);
+    service.execute({ name, description });
+  } catch (err) {
+    return response.status(400).json({ message: err.message });
+  }
+
+  return response.status(201).send();
+});
+
+specificationRoutes.get("/", (request, response) => {
+  const categories = specificationRepository.list();
+  return response.status(200).json(categories);
+});
+
+export { specificationRoutes };
