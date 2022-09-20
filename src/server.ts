37ee6d2b47a8d6ce49120f99f2e38aa747a9943a
Ítalo Holanda <italoholanda@gmail.com.br>
2022-09-20 07:14:02 -0300

    feat: create category documentation

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerOptions from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerOptions));
+
 app.use(router);
 
 app.listen(3333, () => console.log("\nSERVER RUNNING ...\n"));
