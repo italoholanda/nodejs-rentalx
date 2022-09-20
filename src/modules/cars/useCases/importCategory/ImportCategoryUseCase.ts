af4437e8c5f8feea54b1b57e2a903f3ac6711774
Ítalo Holanda <italoholanda@gmail.com.br>
2022-09-20 06:36:00 -0300

    feat: read categories csv file impl

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -3,24 +3,47 @@ import fs from "fs";
 
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
+type MulterFile = Express.Multer.File;
+
 export default class ImportCategoryUseCase {
   constructor(private repository: ICategoriesRepository) {
     this.repository = repository;
   }
 
-  execute(file: Express.Multer.File | undefined) {
-    if (!file) throw new Error("Missing file");
+  private categoryAlreadyExists(name: string) {
+    return Boolean(this.repository.findByName(name));
+  }
 
-    const stream = fs.createReadStream(file.path);
+  private loadCategories(file: MulterFile): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
+      const parseFile = csvParse();
 
-    const parseFile = csvParse();
+      stream.pipe(parseFile);
 
-    stream.pipe(parseFile);
-
-    parseFile.on("data", async (line) => {
-      console.log(line);
+      parseFile
+        .on("data", async (line) => {
+          const [name, description] = await line;
+          categories.push({ name, description });
+        })
+        .on("end", () => resolve(categories))
+        .on("error", (err) => reject(err));
     });
+  }
 
-    console.log(file);
+  async execute(file?: MulterFile) {
+    if (!file) throw new Error("Missing file");
+    const categories = await this.loadCategories(file);
+
+    categories.forEach((category) => {
+      if (this.categoryAlreadyExists(category.name)) return;
+      this.repository.create(category);
+    });
   }
 }
