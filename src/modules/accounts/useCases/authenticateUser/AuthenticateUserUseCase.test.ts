c79c5cee0b922f6006a25f2c5240a08d8ec14f7e
italo <italo.holanda@escolaconquer.com.br>
2022-12-20 12:15:34 -0300

    feat: authentication tests

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.test.ts
@@ -0,0 +1,51 @@
+import { AppError } from "../../../../errors/AppError";
+import User from "../../entities/User";
+import { UserRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import AuthenticateUserUseCase from "./AuthenticateUserUseCase";
+
+let usersRepository: IUsersRepository;
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Authenticate user", () => {
+  const email = "email@example.com";
+  const password = "password";
+
+  beforeEach(async () => {
+    usersRepository = new UserRepositoryInMemory();
+    authenticateUserUseCase = new AuthenticateUserUseCase(usersRepository);
+    createUserUseCase = new CreateUserUseCase(usersRepository);
+
+    const user: User = new User();
+
+    Object.assign(user, {
+      email,
+      password,
+    });
+
+    await createUserUseCase.execute(user);
+  });
+
+  it("Should be able to authenticate", async () => {
+    const res = await authenticateUserUseCase.execute(email, password);
+    expect(res).toHaveProperty("token");
+  });
+
+  it("Should not be able to authenticate with invalid email", () => {
+    expect(async () => {
+      const email = "email2@example.com";
+
+      await authenticateUserUseCase.execute(email, password);
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("Should not be able to authenticate with invalid password", () => {
+    expect(async () => {
+      const password = "1234";
+
+      await authenticateUserUseCase.execute(email, password);
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
