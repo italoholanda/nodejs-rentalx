a611b84cda8f882610aff93a4834bd3316790433
italo <italo.holanda@escolaconquer.com.br>
2022-12-21 15:21:52 -0300

    chore: routes and server dir

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,27 @@
+import "reflect-metadata";
+import express, { Request, Response } from "express";
+import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
+
+import { AppError } from "../../../errors/AppError";
+import swaggerOptions from "../../../swagger.json";
+import { router } from "./routes";
+
+import "../../../database";
+import "../../container";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerOptions));
+
+app.use(router);
+
+app.use((err: Error, request: Request, response: Response) => {
+  if (err instanceof AppError)
+    return response.status(err.statusCode).json(err.message);
+  return response.status(500).json(`Internal server error: ${err.message}`);
+});
+
+app.listen(3333, () => console.log("\nSERVER RUNNING ...\n"));
