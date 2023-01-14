67b0f3eea6fccf03e6aab40f9a9307c39ca88483
italo <italo.holanda@escolaconquer.com.br>
2023-01-14 19:07:21 -0300

    fix: improved function readability

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -33,13 +33,24 @@ class CarsRepositoryInMemory implements ICarsRepository {
   async findAvailable(args: IFindAvailableArgs): Promise<Car[]> {
     const { category_id, name, brand } = args;
 
-    return this.carList
-      .filter((car) => car.available)
-      .filter((car) => !category_id || car.category_id === category_id)
-      .filter((car) => !name || car.name.toLowerCase() === name.toLowerCase())
-      .filter(
-        (car) => !brand || car.brand.toLowerCase() === brand.toLowerCase()
-      );
+    const isCarAvailable = (car: Car) => car.available;
+
+    const checkName = (car: Car, name?: string) =>
+      !name || car.name.toLowerCase() === name.toLowerCase();
+
+    const checkBrand = (car: Car, brand?: string) =>
+      !brand || car.brand.toLowerCase() === brand.toLowerCase();
+
+    const checkCategory = (car: Car, category_id?: string) =>
+      !category_id || car.category_id === category_id;
+
+    return this.carList.filter(
+      (car) =>
+        isCarAvailable(car) &&
+        checkName(car, name) &&
+        checkBrand(car, brand) &&
+        checkCategory(car, category_id)
+    );
   }
 }
 
