3ff388ca0ad3e9d3262e5a5d92d7df9fce089911
italo <italo.holanda@escolaconquer.com.br>
2023-01-30 08:04:39 -0300

    feat: specification controller

--- src/modules/cars/useCases/createCarSpecification/createCarSpecificationUseCase.ts
+++ src/modules/cars/useCases/createCarSpecification/createCarSpecificationUseCase.ts
@@ -1,4 +1,7 @@
+import { inject, injectable } from "tsyringe";
+
 import { AppError } from "../../../../shared/errors/AppError";
+import { Car } from "../../infra/typeorm/entities/Car";
 import { ICarsRepository } from "../../repositories/ICarsRepository";
 import { ISpecificationRepository } from "../../repositories/ISpecificationRepository";
 
@@ -6,16 +9,23 @@ interface IRequest {
   car_id: string;
   specifications_id: string[];
 }
+
+@injectable()
 class CreateCarSpecificationUseCase {
   constructor(
+    @inject("CarsRepository")
     private carsRepository: ICarsRepository,
+
+    @inject("SpecificationRepository")
     private specificationsRepository: ISpecificationRepository
   ) {}
 
-  async execute({ car_id, specifications_id }: IRequest) {
+  async execute({ car_id, specifications_id }: IRequest): Promise<Car> {
     const carExists = await this.carsRepository.findById(car_id);
 
-    if (!carExists) throw new AppError("Car does not exists");
+    if (!carExists) {
+      throw new AppError("Car does not exists!");
+    }
 
     const specifications = await this.specificationsRepository.findByIds(
       specifications_id
@@ -24,6 +34,8 @@ class CreateCarSpecificationUseCase {
     carExists.specifications = specifications;
 
     await this.carsRepository.create(carExists);
+
+    return carExists;
   }
 }
 
--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CreateCarController } from "../../../../modules/cars/useCases/createCar/createCarController";
+import { CreateCarSpecificationController } from "../../../../modules/cars/useCases/createCarSpecification/createCarSpecificationController";
 import { ListAvailableCarsController } from "../../../../modules/cars/useCases/listAvailableCars/listAvailableCarsController";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
@@ -9,6 +10,8 @@ const carRoutes = Router();
 
 const createCarController = new CreateCarController();
 
+const createCarSpecificationController = new CreateCarSpecificationController();
+
 const listAvailableCarsController = new ListAvailableCarsController();
 
 carRoutes.post(
@@ -16,6 +19,13 @@ carRoutes.post(
   ensureAuthenticated,
   ensureAdmin,
   createCarController.handle
+);
+
+carRoutes.post(
+  "/specifications:id",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarSpecificationController.handle
 );
 
 carRoutes.get(
